3cd5c00a92dc0840be3050522f442df7c3390adc
Marco Montenegro <mjmontenegro@gmail.com>
2019-08-05 18:13:55 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace testproj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> myList = new List<object>();
+            myList.Add(7);
+            myList.Add(28);
+            myList.Add(-1);
+            myList.Add(true);
+            myList.Add("chair");
+            int sum = 0;
+            foreach ( object item in myList ) {
+                System.Console.WriteLine(item);
+                if (item is int) {
+                    sum += (int)item;
+                }
+            }
+            System.Console.WriteLine(sum);
+
+        }
+    }
+}
